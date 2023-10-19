781c488f5e182d57739db5e087d5d441250f1a3d
CaioHGuedes <caiocicm@gmail.com>
2023-10-18 21:45:23 -0300

    parametros-url

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Ativar links do menu
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -6,8 +8,19 @@ function ativarLink(link) {
   if (url.includes(href)) {
     link.classList.add("ativo");
   }
-
-  console.log(url);
 }
 
 links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
